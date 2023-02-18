f4f0df48da7bd252b8a36ee5002df4ea64699e0b
Ben Watson <benwatson528@gmail.com>
2023-02-18 14:49:00 +0000

    Day 15 part 2

--- tests/day15/test_timing_is_everything.py
+++ tests/day15/test_timing_is_everything.py
@@ -13,10 +13,16 @@ def test_p1_real():
     assert solve(read_input("data/input.txt")) == 203660
 
 
+def test_p2_real():
+    discs = read_input("data/input.txt")
+    discs.append((len(discs) + 1, 11, 0, 0))
+    assert solve(discs) == 2408135
+
+
 def read_input(file_name):
     with open(os.path.join(Path(__file__).parent.absolute(), file_name)) as f:
         lines = []
         for line in f.read().splitlines():
             raw_nums = re.findall(r"(\d+)", line)
-            lines.append(list(int(num) for num in raw_nums))
+            lines.append(tuple(int(num) for num in raw_nums))
     return lines
