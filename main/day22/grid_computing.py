e0f8f67c2847f55350cd31703080f689c2d136eb
Ben Watson <benwatson528@gmail.com>
2024-12-31 15:19:33 +0000

    Day 21 part 2

--- main/day22/grid_computing.py
+++ main/day22/grid_computing.py
@@ -1,3 +1,4 @@
+import tkinter
 from itertools import combinations
 
 
@@ -7,15 +8,6 @@ def solve_viable(grid) -> int:
         counter += 1 if is_viable(coord_pairs, grid) else 0
         counter += 1 if is_viable((coord_pairs[1], coord_pairs[0]), grid) else 0
     return counter
-
-
-def solve_traverse(grid) -> int:
-    end = max(x for x, _ in grid.keys()), 0
-    return -1
-
-
-#  0     1      2      3
-# Size  Used  Avail  Use%
 
 
 def is_viable(coord_pairs, grid):
@@ -28,3 +20,74 @@ def is_viable(coord_pairs, grid):
     if a_props[1] > b_props[2]:
         return False
     return True
+
+
+def solve_traverse(input_grid) -> int:
+    global start
+    global end
+    global count
+    global grid
+    global clicked
+    start = max(x for x, _ in input_grid.keys()), 0
+    end = 0, 0
+    count = 0
+    grid = input_grid
+    clicked = None
+    root = tkinter.Tk()
+    root.title("Grid Computing")
+    root.geometry("2000x1300")
+
+    frame = tkinter.Frame(root)
+    frame.pack(pady=10)
+
+    max_x, max_y = max(x for x, _ in grid.keys()), max(y for _, y in grid.keys())
+    for x in range(max_x + 1):
+        for y in range(max_y + 1):
+            button = create_button(frame, x, y)
+            button.grid(row=y, column=x)
+
+    root.mainloop()
+    return -1
+
+
+def create_button(frame, x, y):
+    size, used, available, usage_pct = grid[(x, y)]
+    button = tkinter.Button(frame, text=f"{used}/{size}")
+    button.config(height=2, width=5, bg=set_colour(x, y), command=lambda: on_click(button, x, y))
+    return button
+
+
+def set_colour(x, y):
+    if (x, y) == start:
+        return "yellow"
+    elif (x, y) == end:
+        return "green"
+    else:
+        return "white"
+
+
+def on_click(button, x, y):
+    global count, grid, start, end, clicked
+
+    if not clicked:
+        clicked = (button, x, y)
+    else:
+        c_button, c_x, c_y = clicked
+        b_size, b_used, b_available, b_usage_pct = grid[(x, y)]
+        c_size, c_used, c_available, c_usage_pct = grid[(c_x, c_y)]
+
+        if c_used > b_available or (c_x, c_y) == (x, y):
+            clicked = None
+            return
+
+        if (c_x, c_y) == start:
+            start = (c_x, c_y)
+            button["bg"] = "yellow"
+        grid[(x, y)] = b_size, b_used + c_used, b_available - c_used, int(((b_used + c_used) / b_available) * 100)
+        grid[(c_x, c_y)] = c_size, 0, c_size, 0
+        c_button["text"] = f"0/{c_size}"
+        button["text"] = f"{b_used + c_used}/{b_size}"
+
+        clicked = None
+        count += 1
+        print(f"count = {count} moving ({c_x},{c_y}) to ({x},{y})")
--- tests/day22/test_grid_computing.py
+++ tests/day22/test_grid_computing.py
@@ -1,5 +1,8 @@
 import os
+import re
 from pathlib import Path
+
+import pytest
 
 from main.day22.grid_computing import solve_viable, solve_traverse
 
@@ -8,21 +11,15 @@ def test_p1_real():
     assert solve_viable(read_input("data/input.txt")) == 960
 
 
-def test_p2_test():
-    assert solve_traverse(read_input("data/test_input.txt")) == -1
-
-
+@pytest.mark.skip("Requires using the gui")
 def test_p2_real():
-    assert solve_traverse(read_input("data/input.txt")) == -1
+    assert solve_traverse(read_input("data/input.txt")) == 225
 
 
 def read_input(file_name):
     with open(os.path.join(Path(__file__).parent.absolute(), file_name)) as f:
-        split = [line.split() for line in f.read().splitlines()[2:]]
-
         parsed = {}
-        for s in split:
-            x = int(s[0].split("-")[-2][1:])
-            y = int(s[0].split("-")[-1][1:])
-            parsed[(x, y)] = (int(s[1][:-1]), int(s[2][:-1]), int(s[3][:-1]), int(s[4][:-1]))
+        for l in f.readlines():
+            nums = [int(x) for x in re.findall(r"\d+", l)]
+            parsed[(nums[0], nums[1])] = tuple(nums[2:])
         return parsed
